b1871a4a017013baebb63de932d5d0f03a182bad
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-08-23 17:47:09 +0200

    commit-four

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
+import { MainStyle } from 'components/App.style';
 import { Profile } from 'components/Profile/Profile';
-import user from 'json.files/user.json';
 import { Statistic } from 'components/Statistics/Statistics';
-import data from 'json.files/data.json';
 import { FriendList } from 'components/FriendList/FriendList';
-import friendsList from 'json.files/friends.json'
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import transaction from 'json.files/transactions.json'
-import {MainStyle} from 'components/App.style'
+import user from 'json.files/user.json';
+import data from 'json.files/data.json';
+import friendsList from 'json.files/friends.json';
+import transaction from 'json.files/transactions.json';
+
 export const App = () => {
   return (
     <MainStyle>
       <Profile users={user} />
       <Statistic title="Upload stats" stats={data} />
-      <FriendList friends={friendsList}  />
+      <FriendList friends={friendsList} />
       <TransactionHistory transactions={transaction} />
     </MainStyle>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { Profile } from 'components/Profile/Profile';
-import { Statistic} from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
